a7021ca0966bc18e87d1193704666ef8a40b09c9
frsw3nr <frsw3nr@gmail.com>
2017-02-04 06:59:21 +0900

    testing compare v0.1.7

---
+++ src/main/resources/root/en/config/test_servers.groovy
@@ -0,0 +1,13 @@
+// Target Servers
+
+server_infos = [
+    [
+        server_name :       'localhost',
+        ip :                'localhost',
+        platform :          'Linux',
+        os_account_id :     'Test',
+        remote_account_id : '',
+        remote_alias :      '',
+        verify_id :         '',
+    ],
+]
