977376e92ee948ed4ae3eaaa35dc508b31214485
Minoru Furusawa <minoru.furusawa@toshiba.co.jp>
2017-02-06 16:40:03 +0900

    v0.1.7

--- src/main/resources/root/jp/config/cmdb.groovy
+++ src/main/resources/root/jp/config/cmdb.groovy
@@ -2,5 +2,5 @@
 
 cmdb.dataSource.username = "root"
 cmdb.dataSource.password = "getperf"
-cmdb.dataSource.url = "jdbc:mysql://localhost:3306/cmdb"
+cmdb.dataSource.url = "jdbc:mysql://localhost:3306/cmdb?useUnicode=true&characterEncoding=utf8"
 cmdb.dataSource.driver = "com.mysql.jdbc.Driver"
