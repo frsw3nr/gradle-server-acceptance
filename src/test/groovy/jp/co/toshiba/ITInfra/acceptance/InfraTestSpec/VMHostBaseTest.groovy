4dd087e443519e71051698a079b76e54799f0793
frsw3nr <frsw3nr@gmail.com>
2017-03-18 07:08:01 +0900

    v0.1.9

--- src/test/groovy/jp/co/toshiba/ITInfra/acceptance/InfraTestSpec/VMHostBaseTest.groovy
+++ src/test/groovy/jp/co/toshiba/ITInfra/acceptance/InfraTestSpec/VMHostBaseTest.groovy
@@ -12,7 +12,9 @@ class VMHostBaseTest extends Specification {
     def setup() {
         test_server = new TargetServer(
             server_name       : 'esxi001',
+            ip                : '192.168.10.100',
             platform          : 'VMHost',
+            os_account_id     : 'Test',
             remote_account_id : 'Test',
             remote_alias      : 'esxi001',
         )
@@ -20,12 +22,20 @@ class VMHostBaseTest extends Specification {
         test_server.dry_run = true
     }
 
+        // vcenter_ip       = test_server.ip
+        // def os_account   = test_server.os_account
+        // vcenter_user     = os_account['user']
+        // vcenter_password = os_account['password']
+        // vm               = test_server.ip
+
+
     def "VMHost テスト仕様 NetworkAdapter"() {
         setup:
         test = new DomainTestRunner(test_server, 'VMHost')
 
         when:
         def test_item = new TestItem('NetworkAdapter')
+println test_item.test_id
         test.run(test_item)
 
 println test_item.results
