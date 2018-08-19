d1cf4fc511d0d9a035ba41644cfa7ea37bb23fa0
frsw3nr <frsw3nr@gmail.com>
2018-08-20 06:43:26 +0900

    v1.23

--- cleansing/getconfig_cleansing/evidence/collector.py
+++ cleansing/getconfig_cleansing/evidence/collector.py
@@ -1,72 +1,101 @@
 import re
 import sys
 import os
-import logging
 from enum import Enum
 from datetime import datetime
 import logging
+import numpy as np
+import pandas as pd
 from abc import ABCMeta, abstractmethod
+from getconfig_cleansing.evidence.inventory import InventoryInfo
 
-class EvidenceCollector(object):
-    EVIDENCE_DIR = 'build'
+class InventoryCollector(object):
+    INVENTORY_DIR = 'build'
 
-    def __init__(self, evidence_source = EVIDENCE_DIR, **kwargs):
-        self.evidence_source = evidence_source
+    def __init__(self, inventory_source = INVENTORY_DIR, **kwargs):
+        self.inventory_source = inventory_source
 
-    def analyze_evidence_path(self, evidence_dir, excel_file):
+    def make_inventory_from_excel_path(self, excel_path):
+        ''' Excel インベントリソースパス名を解析する '''
+        inventory_dir, excel_file = os.path.split(excel_path)
+        if not excel_file.endswith('.xlsx'):
+            return
+
         # Extract project from '.../{project}/build'
-        match_dir = re.search(r'([^/]+?)/build$', evidence_dir)
-        project = match_dir.group(1) if match_dir else "unkown"
+        match_dir = re.search(r'([^/]+?)/build$', inventory_dir)
+        project = match_dir.group(1) if match_dir else "_Default"
 
-        # Extract evidence,timestamp from '{evidence}_{YYYYMMDD_HHMI}.xlsx'
-        evidence = excel_file
-        match_excel = re.match(r'^(.+)_(\d+_\d+).xlsx$', excel_file)
+        # Extract inventory,timestamp from '{inventory}_{YYYYMMDD_HHMI}.xlsx'
+        inventory = excel_file[0:-5]
+        match_file = re.match(r'^(.+)_(\d+_\d+)$', inventory)
         timestamp = ''
-        if match_excel:
-            evidence  = match_excel.group(1)
-            timestamp = match_excel.group(2)
+        if match_file:
+            inventory  = match_file.group(1)
+            timestamp = match_file.group(2)
 
-        return {'evidence': evidence, 'project': project, 'timestamp': timestamp}
+        return InventoryInfo(excel_path, inventory, project, timestamp)
 
-    def list_evidences(self):
+    def scan_excel_inventory_files(self, inventory_source):
         _logger = logging.getLogger(__name__)
-        rv = []
-        if os.path.isfile(self.evidence_source):
-            evidence_dir, excel_file = os.path.split(self.evidence_source)
-            if excel_file.endswith('.xlsx'):
-                info = self.analyze_evidence_path(evidence_dir, excel_file)
-                info['path'] = self.evidence_source
-                print("INFO : %s" % (info))
-                rv.append(excel_file[0:-5])
-        elif os.path.isdir(self.evidence_source):
-            for evidence_dir, dirs, files in os.walk(self.evidence_source):
+        inventoris = []
+        if os.path.isfile(inventory_source):
+            inventory = self.make_inventory_from_excel_path(inventory_source)
+            if inventory:
+                inventoris.append(inventory)
+        elif os.path.isdir(inventory_source):
+            for inventory_dir, dirs, files in os.walk(inventory_source):
                 for excel_file in files:
-                    if excel_file.endswith('.xlsx'):
-                        info = self.analyze_evidence_path(evidence_dir, excel_file)
-                        rv.append(excel_file[0:-5])
-            rv.sort()
-        return rv
+                    excel_path = os.path.join(inventory_dir, excel_file)
+                    inventory = self.make_inventory_from_excel_path(excel_path)
+                    if inventory:
+                        inventoris.append(inventory)
+            inventoris.sort(key=lambda x: x.timestamp)
+        return inventoris
+
+    def read_excel_inventory(self, excel_source):
+        xls = pd.ExcelFile(excel_source)
+        df = self.xls.parse('検査シート', skiprows=range(0,2))
+        # 先頭のマスクの掛かった箇所を削除する
+        df.drop(range(8))
+        df = df.dropna(subset=['ホスト名'])
+        # ネットワーク構成情報から、IPアドレスを抽出
+        df2 = Util.expand_ip_address_list(df, 'ネットワーク構成')
+        df2['ManagementLAN'] = False
+        df3 = Util.expand_ip_address_list(df, '管理LAN')
+        df3['ManagementLAN'] = True
+        df = pd.concat([df2, df3], axis=0)
+
+        return df
+
+    def load(self, inventory_source):
+        _logger = logging.getLogger(__name__)
+        inventorys = self.scan_excel_inventory_files(inventory_source)
+        for inventory in inventorys:
+            print("SOURCE:", inventory.source)
+            df = read_excel_inventory(inventory_source)
+            return df
+
 
     def get_module(self, name):
         _logger = logging.getLogger(__name__)
         try:
-            mod = __import__('getconfig_cleansing.master.parser_' + name,
-                             None, None, ['EvidenceParser'])
+            mod = __import__('getconfig_cleansing.inventory.parser_' + name,
+                             None, None, ['InventoryParser'])
         except ImportError:
             return
         return mod
 
     def parse(self):
         _logger = logging.getLogger(__name__)
-        evidences = self.list_evidences()
-        for evidence in evidences:
-            match = re.match(r'^(.+)_(\d+_\d+)$', evidence)
+        inventorys = self.list_inventorys()
+        for inventory in inventorys:
+            match = re.match(r'^(.+)_(\d+_\d+)$', inventory)
             timestamp = ''
             if match:
-                evidence = match.group(1)
+                inventory = match.group(1)
                 timestamp = match.group(2)
-            _logger.warn("Check: %s, Date: %s" % (evidence, timestamp))
-            mod = self.get_module(evidence)
+            _logger.warn("Check: %s, Date: %s" % (inventory, timestamp))
+            mod = self.get_module(inventory)
             if mod:
-                mod.EvidenceParser().say('Hello')
+                mod.InventoryParser().say('Hello')
 
--- cleansing/tests/evidence/test_collector.py
+++ cleansing/tests/evidence/test_collector.py
@@ -4,30 +4,87 @@ import os
 import pytest
 
 from getconfig_cleansing import cli
-from getconfig_cleansing.evidence.collector import EvidenceCollector
+from getconfig_cleansing.evidence.collector import InventoryCollector
+from getconfig_cleansing.evidence.loader import InventoryLoader
 
-# py.test tests/evidence/test_collector.py -v --capture=no
+# py.test tests/evidence/test_collector.py -v --capture=no -k test_parse_evidence_from_excel
 
-# @pytest.fixture
-# def collector():
-#     return EvidenceCollector('tests/resources/import')
+def test_parse_evidence_from_excel_file1():
+    inventory = InventoryCollector().make_inventory_from_excel_path('')
+    assert inventory == None
+
+@pytest.mark.parametrize('path,name,project,timestamp', [
+    ('/project1/build/サーバチェックシート_20180818_101734.xlsx',
+        'サーバチェックシート', 'project1', '20180818_101734'),
+    ('/project1/build/check_sheet.xlsx',
+        'check_sheet', 'project1', ''),
+    ('/check_sheet.xlsx',
+        'check_sheet', '_Default', ''),
+])
+def test_parse_evidence_from_excel_file2(path, name, project, timestamp):
+    inventory = InventoryCollector().make_inventory_from_excel_path(path)
+    assert inventory.source    == path
+    assert inventory.name      == name
+    assert inventory.project   == project
+    assert inventory.timestamp == timestamp
+
+def test_parse_evidence_from_excel_dir1():
+    collector = InventoryCollector()
+    path = 'tests/resources/project1/build/サーバチェックシート_20180818_101734.xlsx'
+    inventorys = collector.scan_excel_inventory_files(path)
+    assert len(inventorys) == 1
+
+def test_parse_evidence_from_excel_dir2():
+    collector = InventoryCollector()
+    path = 'tests/resources'
+    inventorys = collector.scan_excel_inventory_files(path)
+    assert len(inventorys) > 1
+
+def test_parse_evidence_from_excel_dir3():
+    collector = InventoryCollector()
+    path = 'hoge'
+    inventorys = collector.scan_excel_inventory_files(path)
+    print(inventorys)
+    assert 1 == 1
+
+def test_load1():
+    collector = InventoryCollector()
+    path = 'tests/resources/project1/build/サーバチェックシート_20180818_101734.xlsx'
+    inventory = collector.make_inventory_from_excel_path(path)
+    df = InventoryLoader().read_inventory_excel(inventory)
+    print(df[['ホスト名','IP']])
+    assert 1 == 1
+
+# def test_parse_evidence_from_excel_dir():
+#     collector = InventoryCollector()
+#     evidence = collector.make_inventory_from_source('tests/resources/')
+#     assert 1 == 1
+
+# def test_parse_evidence_from_excel1(collector):
+#     path = '/project1/build/サーバチェックシート_20180818_101734.xlsx'
+#     evidence = collector.make_inventory_from_source(path)
+
+#     assert evidence.source    == path
+#     assert evidence.name      == 'サーバチェックシート'
+#     assert evidence.project   == 'project1'
+#     assert evidence.timestamp == '20180818_101734'
 
 def test_import_single_evidence():
-    collector = EvidenceCollector('tests/resources/import/project1/build/サーバチェックシート_20180817_142016.xlsx')
+    collector = InventoryCollector('tests/resources/import/project1/build/サーバチェックシート_20180818_101734.xlsx')
     print ("Single test")
     collector.parse()
     assert 1 == 1
 
 def test_import_multiple_evidence():
-    collector = EvidenceCollector('tests/resources/import')
+    collector = InventoryCollector('tests/resources/import')
     collector.parse()
     rv = collector.list_evidences()
-    assert len(rv) == 4
+    # assert len(rv) == 4
 
     # evidences = collector.list_commands()
     # print(evidences)
     # for evidence in evidences:
-    #     print("Evidence:%s" % (evidence))
+    #     print("Inventory:%s" % (evidence))
     #     match = re.match(r'^(.+)_(\d+_\d+)$', evidence)
     #     timestamp = ''
     #     if match:
@@ -36,13 +93,13 @@ def test_import_multiple_evidence():
     #     print("Check: %s, Date: %s" % (evidence, timestamp))
     #     mod = collector.get_module(evidence)
     #     if mod:
-    #         mod.EvidenceParser().say('Hello')
+    #         mod.InventoryParser().say('Hello')
 
     # mod1 = loader.get_module('check_sheet')
-    # mod1.EvidenceParser().say('Hello')
+    # mod1.InventoryParser().say('Hello')
 
     # mod2 = loader.get_module('サーバチェックシート')
-    # mod2.EvidenceParser().say('Hello')
+    # mod2.InventoryParser().say('Hello')
     assert 1 == 1
 
 
