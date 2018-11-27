9cbdd5b22f557f75676a96bd719f37318ec91877
Minoru Furusawa <minoru.furusawa@toshiba.co.jp>
2018-11-27 16:50:36 +0900

    v1.33

--- src/test/groovy/jp/co/toshiba/ITInfra/acceptance/Ticket/TestRedmineRepository.groovy
+++ src/test/groovy/jp/co/toshiba/ITInfra/acceptance/Ticket/TestRedmineRepository.groovy
@@ -10,6 +10,57 @@ import jp.co.toshiba.ITInfra.acceptance.Ticket.*
 // setenv REDMINE_API_KEY={APIキー}
 // gradle --daemon test --tests "TestRedmineRepository.デモ1"
 
+/*
+ToDo
+=====
+
+Redmine I/F作成
+---------------
+
+redmine_repository.py RedmineRepository
+redmine_field.py      RedmineField
+redmine_stat.py       RedmineStatistics 必要？
+redmine_cache.py      RedmineStatistics 必要？
+
+ticket.py　メイン処理
+
+    def set_custom_fields(self):
+        Redmine カスタムフィールドのIDリストと、カスタムフィールドからCSVカラム名の逆引きリストの生成
+    def get_issue_cache(self, key, **kwargs):
+        SQLite3 キャッシュからキーを指定してチケット検索
+    def reset_record_statistics(self):
+        チケット登録処理統計の初期化
+    def validate_custom_fileds(self):
+        チケット登録処理統計から、全カスタムフィールドが設定されているかを返す
+    def get_difference_with_cache(self, issue_cache, csv):
+        チケット属性のキャッシュ値とCSV値の比較をする。
+    def get_custom_field_default_value(self, field_name, row, **kwargs):  ※未使用
+    def get_issue_status_id(self, issue, **kwargs):
+        チケットのステータスID取得。
+    def get_user_updated_fields(self, issue):
+        チケットカスタムフィールドのユーザ更新有無をチェックする。
+    def make_custom_fields(self, row, csv, user_updated_fields = {}):
+        登録するチケットのカスタムフィールド値を設定
+    def regist(self, fab, key, row, **kwargs):
+        Redmine チケットの登録。use_cache, skip_redmine オプションの条件により処理が変わる
+
+単体テスト
+----------
+
+jp/co/toshiba/ITInfra/acceptance/Ticket
+
+import jp.co.toshiba.ITInfra.acceptance.Ticket.RedmineRepository
+import jp.co.toshiba.ITInfra.acceptance.Ticket.RedmineField
+import jp.co.toshiba.ITInfra.acceptance.Ticket
+
+lib/Ticket/AIX
+lib/Ticket/Eternus
+lib/Ticket/HitachiVSP
+lib/Ticket/iLO
+lib/Ticket/Linux
+*/
+
+
 class TestRedmineRepository extends Specification {
     def manager
 
